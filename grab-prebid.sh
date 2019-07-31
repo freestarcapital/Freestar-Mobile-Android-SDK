60cfdf6d7c8a1bbc05287ebbe2b012517ad82ffa
Brian Sorensen <brian.sorensen@freestar.io>
2019-07-30 22:37:57 -0700

    split out pom files

--- grab-prebid.sh
+++ grab-prebid.sh
@@ -2,5 +2,6 @@
 
 #ls ~/master_project/workspace/prebid-mobile-android/PrebidMobile/API1.0/build/outputs/aar/API1.0-release.aar 
 
-mvn install:install-file -DgroupId=freestar-mobile -DartifactId=prebid-api-1.0 -Dversion=FS-1.0.0 -Dfile=/Users/bsorensen/master_project/workspace/prebid-mobile-android/PrebidMobile/API1.0/build/outputs/aar/API1.0-release.aar -Dpackaging=aar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+#mvn install:install-file -DgroupId=freestar-mobile -DartifactId=prebid-api-1.0 -Dversion=FS-1.0.0 -Dfile=/Users/bsorensen/master_project/workspace/prebid-mobile-android/PrebidMobile/API1.0/build/outputs/aar/API1.0-release.aar -Dpackaging=aar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
+mvn install:install-file -DgroupId=freestar-mobile -DartifactId=prebid-api-1.0 -Dversion=FS-1.0.0 -Dfile=/Users/bsorensen/master_project/workspace/prebid-mobile-android/PrebidMobile/API1.0/build/outputs/aar/API1.0-release.aar -Dpackaging=aar -DpomFile=prebid_pom.xml -DlocalRepositoryPath=.  -DcreateChecksum=true
 
