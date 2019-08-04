f6360d22c7b9780bf5e69d12fb86468e6e4454f0
Brian Sorensen <brian.sorensen@freestar.io>
2019-08-03 21:23:00 -0700

    reset

--- grab-freestar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#mvn install:install-file -DgroupId=freestar-mobile -DartifactId=freestar-api -Dversion=1.0.0 -Dfile=/Users/bsorensen/master_project/workspace/freestar-android-sdk/freestarSDK/build/outputs/aar/freestarSDK-release.aar -Dpackaging=aar -DgeneratePom=true -DlocalRepositoryPath=.  -DcreateChecksum=true
-mvn install:install-file -DgroupId=freestar-mobile -DartifactId=freestar-api -Dversion=1.0.0 -Dfile=/Users/bsorensen/master_project/workspace/freestar-android-sdk/freestarSDK/build/outputs/aar/freestarSDK-release.aar -Dpackaging=aar -DpomFile=freestar_pom.xml -DlocalRepositoryPath=.  -DcreateChecksum=true
-
