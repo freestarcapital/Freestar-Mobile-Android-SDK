99c59c81bf9495e38f3c1d60510f8ce40bd2e250
Brian Sorensen <brian.sorensen@freestar.io>
2019-11-14 14:54:51 -0700

    interim to v1.0.3

--- app/src/main/java/com/freestar/android/examples/example3/MainActivity.java
+++ app/src/main/java/com/freestar/android/examples/example3/MainActivity.java
@@ -5,18 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.freestar.android.sdk.adslot.DfpAdSlot;
+import com.freestar.android.sdk.domain.DemandFetcher;
+import com.freestar.android.sdk.model.FreestarAdModel;
+import com.freestar.android.sdk.model.FreestarInteraction;
+import com.freestar.android.sdk.model.FreestarViewInjector;
+import com.freestar.android.sdk.model.InjectorProperties;
+import com.freestar.android.sdk.widget.holder.FreestarAdListener;
+
 public class MainActivity extends AppCompatActivity {
+    private static final boolean interactionSuccess = new FreestarInteraction.Builder()
+            .addDisplayOption("<html><body><h2 style='color:red;font-size:40px'>Your patronage helps us better support you</h2></body></html>")
+            .addDisplayOption("<html><body><h2 style='color:red;font-size:40px'>Please support our sponsors</h2></body></html>")
+            .addDisplayOption("<html><body><h2 style='color:red;font-size:40px'>Ads support this site</h2></body></html>")
+            .addDisplayOption("<html><body><h2 style='color:green;font-size:40px'>Ads powered by Freestar</h2></body></html>")
+            .addClickSupportOption("\"<html><body><h1 style='color:orange;font-size:40px'>Thank-you for your support</h1></body></html>\"")
+            .load();
+
+    private static DfpAdSlot adSlotA = new DfpAdSlot.Builder()
+            .setDfpPlacementId("/15184186/freestar_androidapp_320x50_ATF")
+            .addSize(320, 50)
+            .addCustomTarget("custom1", "value2")
+            .addCustomTarget("custom2", "value1")
+            .setAutoRefreshSeconds(30)
+            .build();
+
+    private static DfpAdSlot adSlotB = new DfpAdSlot.Builder()
+            .setDfpPlacementId("/15184186/freestar_androidapp_300x250_InContent")
+            .addSize(300, 250)
+            .addCustomTarget("custom1", "value2")
+            .addCustomTarget("custom2", "value1")
+            .setAutoRefreshSeconds(30)
+            .build();
 
     private View textViewAd1A;
     private View textViewAd1B;
+    DemandFetcher f;
+    FreestarAdListener lis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FreestarAdModel.getInstance(this);
         setContentView(R.layout.activity_main);
 
         textViewAd1A = findViewById(R.id.text_view_ad_1a);
@@ -28,6 +65,21 @@ public class MainActivity extends AppCompatActivity {
                 activityJump();
             }
         });
+
+        ViewGroup adViewA = findViewById(R.id.ad_container1A);
+        adViewA.setBackgroundColor(Color.parseColor("#00ff00"));
+        FreestarViewInjector injectorA = FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA);
+        injectorA.injectBannerAd(
+                adViewA,
+                new InjectorProperties.Builder().build(),
+                adSlotA);
+        ViewGroup adViewB = findViewById(R.id.ad_container1B);
+        adViewB.setBackgroundColor(Color.parseColor("#00ff00"));
+        FreestarViewInjector injectorB = FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB);
+        injectorB.injectBannerAd(
+                adViewB,
+                new InjectorProperties.Builder().build(),
+                adSlotB);
     }
 
     private void activityJump() {
@@ -44,4 +96,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA).resumeAd();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB).resumeAd();
+    }
+
+    @Override
+    protected void onPause() {
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA).pauseAd();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB).pauseAd();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA).destroyAd();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB).destroyAd();
+        FreestarAdModel.releaseInstance(this);
+        super.onDestroy();
+    }
 }
--- app/src/main/java/com/freestar/android/examples/example3/SecondaryActivity.java
+++ app/src/main/java/com/freestar/android/examples/example3/SecondaryActivity.java
@@ -1,14 +1,36 @@
 package com.freestar.android.examples.example3;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.freestar.android.sdk.adslot.DfpAdSlot;
+import com.freestar.android.sdk.model.FreestarAdModel;
+import com.freestar.android.sdk.model.FreestarViewInjector;
+import com.freestar.android.sdk.model.InjectorProperties;
+
 public class SecondaryActivity extends AppCompatActivity {
+    private static DfpAdSlot adSlotA = new DfpAdSlot.Builder()
+            .setDfpPlacementId("/15184186/freestar_androidapp_300x250_InContent")
+            .addSize(300, 250)
+            .addCustomTarget("custom1", "value2")
+            .addCustomTarget("custom2", "value1")
+            .setAutoRefreshSeconds(30)
+            .build();
+
+    private static DfpAdSlot adSlotB = new DfpAdSlot.Builder()
+            .setDfpPlacementId("/15184186/freestar_androidapp_320x50_ATF")
+            .addSize(320, 50)
+            .addCustomTarget("custom1", "value2")
+            .addCustomTarget("custom2", "value1")
+            .setAutoRefreshSeconds(30)
+            .build();
 
     private View textViewAd2A;
     private View textViewAd2B;
@@ -16,6 +38,7 @@ public class SecondaryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FreestarAdModel.getInstance(this);
         setContentView(R.layout.activity_secondary);
 
         textViewAd2A = findViewById(R.id.text_view_ad_2a);
@@ -27,6 +50,21 @@ public class SecondaryActivity extends AppCompatActivity {
                 activityJump();
             }
         });
+
+        ViewGroup adViewA = findViewById(R.id.ad_container2A);
+        adViewA.setBackgroundColor(Color.parseColor("#00aa00"));
+        FreestarViewInjector injectorA = FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA);
+        injectorA.injectBannerAd(
+                adViewA,
+                new InjectorProperties.Builder().build(),
+                adSlotA);
+        ViewGroup adViewB = findViewById(R.id.ad_container2B);
+        adViewB.setBackgroundColor(Color.parseColor("#00aa00"));
+        FreestarViewInjector injectorB = FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB);
+        injectorB.injectBannerAd(
+                adViewB,
+                new InjectorProperties.Builder().build(),
+                adSlotB);
     }
 
     private void activityJump() {
@@ -41,5 +79,27 @@ public class SecondaryActivity extends AppCompatActivity {
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA).resumeAd();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB).resumeAd();
+    }
+
+    @Override
+    protected void onPause() {
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA).pauseAd();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB).pauseAd();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotA).destroyAd();
+        FreestarAdModel.getInstance(this).lookupViewInjector(adSlotB).destroyAd();
+        FreestarAdModel.releaseInstance(this);
+        super.onDestroy();
     }
 }
